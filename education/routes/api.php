1b2c20a04df2608b1ce209f527f3c89584225556
saramemic <sm20200111@student.fon.bg.ac.rs>
2024-01-18 13:07:38 +0100

    course-controller-and-routes

--- education/routes/api.php
+++ education/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\Authorization\AuthController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,16 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/courses', [CourseController::class, 'index']);
+Route::get('/courses/pg', [CourseController::class, 'indexPg']);
+Route::get('/courses/{id}', [CourseController::class, 'show']);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/profile', function (Request $request) {
         return auth()->user();
     });
-    
+
     Route::post('/logout', [AuthController::class, 'logout']);
 });
