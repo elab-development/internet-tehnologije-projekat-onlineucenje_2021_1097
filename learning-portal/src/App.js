c8cf15c23dd2039bc2dfd35ca066a0a15960c12d
saramemic <sm20200111@student.fon.bg.ac.rs>
2024-04-16 22:26:39 +0200

    router

--- learning-portal/src/App.js
+++ learning-portal/src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import './App.css';
+import Header from './components/Header/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Courses from './pages/Courses';
+import Team from './pages/Team';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-   <div>Open learning-portal</div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/courses' element={<Courses />} />
+        <Route path='/team' element={<Team />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
